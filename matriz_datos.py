cf6534e3bd3c6e984693a1c36f609d719b6dcc26
Karirut <89891435+Karirut@users.noreply.github.com>
2022-05-10 11:24:21 -0500

    Update matriz_datos.py

--- matriz_datos.py
+++ matriz_datos.py
@@ -1,14 +1,16 @@
 import csv
+import numpy as np
 
 with open('avocado.csv') as file:
     content = file.readlines()
+matrix=np.asmatrix(content)
 header = content[:1]
 rows = content[1:]
 print(header)
 #print(rows)
-
+arra=np.array(header)
 for i in range(len(rows)):
     row=rows[i]
     nrow=row.split(", ")
     print(nrow)
-
+    
